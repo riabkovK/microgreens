0ec0151eebe2b5e0e444938296559b7f735dece3
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-13 11:59:50 +0200

    Added autogenerated IDs in db tables

--- db_migration/sql/000001_init.up.sql
+++ db_migration/sql/000001_init.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email varchar(255) NOT NULL,
     name varchar(255) NOT NULL,
     username varchar(255) UNIQUE NOT NULL,
@@ -7,25 +7,25 @@ CREATE TABLE users (
 );
 
 CREATE TABLE microgreens_family (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE microgreens_list (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
     microgreens_family_id int REFERENCES microgreens_family(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE microgreens_item (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
     microgreens_family_id int REFERENCES microgreens_family(id) ON DELETE CASCADE NOT NULL ,
     price numeric NOT NULL
 );
 
 CREATE TABLE users_microgreens_lists (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id int REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     microgreens_list_id int REFERENCES microgreens_list(id) ON DELETE CASCADE NOT NULL
 );
