3a47dc414fd5d21452ad5da721e13e1615ca1d40
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-13 11:58:35 +0200

    Removed validator sha256 from User struct. Added db flag to UserId

--- internal/user.go
+++ internal/user.go
@@ -1,9 +1,9 @@
 package internal
 
 type User struct {
-	Id           int    `json:"-"`
-	Email        string `json:"email" validate:"required,email"`
-	Name         string `json:"name" validate:"required"`
-	Username     string `json:"username" validate:"required,max=25"`
-	PasswordHash string `json:"password" validate:"required,sha256"`
+	Id       int    `json:"-" db:"id"`
+	Email    string `json:"email" validate:"required,email"`
+	Name     string `json:"name" validate:"required"`
+	Username string `json:"username" validate:"required,max=25"`
+	Password string `json:"password" validate:"required"`
 }
