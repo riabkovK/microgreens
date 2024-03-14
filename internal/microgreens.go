b3a4a0a099bd54cf6587a606a36b2e454cc71cfd
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-14 15:11:13 +0200

    Refactored microgreens.go: - fixed json name for microgreensFamilyId) - added structure to update microgreens list and validator to this structure

--- internal/microgreens.go
+++ internal/microgreens.go
@@ -1,10 +1,12 @@
 package internal
 
+import "errors"
+
 type MicrogreensList struct {
 	Id                  int    `json:"id" db:"id"`
 	Name                string `json:"name" db:"name" validate:"required"`
 	Description         string `json:"description" db:"description"`
-	MicrogreensFamilyId int    `json:"microgreensFamilyId" db:"microgreens_family_id"`
+	MicrogreensFamilyId int    `json:"microgreens_family_id" db:"microgreens_family_id"`
 }
 
 type MicrogreensFamily struct {
@@ -16,7 +18,7 @@ type MicrogreensItem struct {
 	Id                  int    `json:"id" db:"id" `
 	Name                string `json:"name" db:"name" validate:"required"`
 	Description         string `json:"description" db:"description"`
-	MicrogreensFamilyId int    `json:"microgreensFamilyId" db:"microgreens_family_id"`
+	MicrogreensFamilyId int    `json:"microgreens_family_id" db:"microgreens_family_id"`
 	Price               int    `json:"price" db:"price" validate:"required"`
 }
 
@@ -24,4 +26,17 @@ type UsersMicrogreensList struct {
 	Id                int
 	UserId            int
 	MicrogreensListId int
+}
+
+type UpdateMicrogreensListRequest struct {
+	Name                *string `json:"name"`
+	Description         *string `json:"description"`
+	MicrogreensFamilyId *int    `json:"microgreens_family_id"`
+}
+
+func (receiver UpdateMicrogreensListRequest) Validate() error {
+	if receiver.Name == nil && receiver.Description == nil && receiver.MicrogreensFamilyId == nil {
+		return errors.New("update microgreensList structure has no values")
+	}
+	return nil
 }
