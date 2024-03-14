8a701803cbad608b5416ae324a626677ce773015
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-14 10:57:23 +0200

    Added mock handler to create microgreensList

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,9 +1,29 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/riabkovK/microgreens/internal"
+	"github.com/sirupsen/logrus"
+	"strconv"
+)
 
 func (h *Handler) createList(c *fiber.Ctx) error {
-	return nil
+	id, err := c.Locals(userCtx).(int)
+	if !err {
+		return newErrorResponse(c, fiber.StatusInternalServerError, "user id from context is not type of int")
+	}
+	if id == 0 {
+		return newErrorResponse(c, fiber.StatusInternalServerError, "user id not found")
+	}
+
+	request := internal.MicrogreensList{}
+	if err := c.BodyParser(&request); err != nil {
+		return newErrorResponse(c, fiber.StatusBadRequest, err.Error())
+	}
+
+	// call service method
+	logrus.Warning("i'm here. Id is - %v", id)
+	return c.Status(fiber.StatusCreated).JSON(signInResponse{AccessToken: strconv.Itoa(id)})
 }
 
 func (h *Handler) getAllList(c *fiber.Ctx) error {
