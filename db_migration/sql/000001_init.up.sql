bf053f9c7b58ac91ace83c2af3dceb42d34c126e
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-18 16:59:10 +0200

    Fixed migration

--- db_migration/sql/000001_init.up.sql
+++ db_migration/sql/000001_init.up.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
 
 CREATE TABLE microgreens_family (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) UNIQUE NOT NULL
+    name varchar(255) UNIQUE NOT NULL,
+    description varchar(255)
 );
 
 CREATE TABLE microgreens_list (
@@ -35,5 +36,11 @@ CREATE TABLE users_microgreens_lists (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id int REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     microgreens_list_id int REFERENCES microgreens_list(id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE microgreens_family_items (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    microgreens_family_id int REFERENCES microgreens_family(id) ON DELETE CASCADE NOT NULL,
+    microgreens_item_id int REFERENCES microgreens_item(id) ON DELETE CASCADE NOT NULL
 );
 
