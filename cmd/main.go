2cd891f03fb4f2c412d05a20dec57a21c334466c
Konstantin Riabkov <riabkov.k@gmail.com>
2024-03-19 10:56:00 +0200

    Added Todo

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,15 @@ import (
 	"github.com/spf13/viper"
 )
 
+// TODO Добавить:
+// 1) рефреш токен
+// 2) сборку бинаря + проброс конфига (соответственно, флаги), поиск переменных из окружения (viper)
+// 3) docker compose,
+// 4) Тесты
+// 5) swagger
+// 6) CI?
+// 7) UI (на реакте)
+
 func main() {
 	if err := initConfig(); err != nil {
 		logrus.Fatalf("error initializing configs: %v", err)
